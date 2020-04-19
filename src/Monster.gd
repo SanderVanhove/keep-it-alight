e6652e4178336df81b41c04130c420382ef5957c
Lamasaurus <sandervhove@gmail.com>
2020-04-19 09:24:13 +0200

    Add heartbeat to monster

--- src/Monster.gd
+++ src/Monster.gd
@@ -6,7 +6,11 @@ var is_active = false
 var target = null
 var start_position = null
 
+var slowHearthBeat = preload("res://audio/player/heartbeat_slow.wav")
+var fastHearthBeat = preload("res://audio/player/heartbeat_fast.wav")
+
 onready var light = $Light
+onready var heartBeatPlayer = $HeartBeatPlayer
 
 
 func _ready():
@@ -19,23 +23,33 @@ func _process(delta):
 	if is_active:
 		var vector = (target.global_position - position).normalized() * MOVEMENT_SPEED
 		move_and_slide(vector)
+		
 
 
 func _on_MonsterDetection_area_entered(area):
 	if area.get_name() == "VisibilityArea":
 		target = area
 		is_active = true
+		
+		heartBeatPlayer.stream = fastHearthBeat
+		heartBeatPlayer.play()
 
 
 func _on_MonsterDetection_area_exited(area):
 	if area.get_name() == "VisibilityArea":
 		is_active = false
+		
+		heartBeatPlayer.stream = slowHearthBeat
+		heartBeatPlayer.play()
 
 
 func _on_MonsterDetection_body_entered(body):
 	if body.get_name() == "Player":
 		body.die()
 		is_active = false
+		
+		heartBeatPlayer.stream = slowHearthBeat
+		heartBeatPlayer.play()
 		
 		yield(get_tree().create_timer(1.5), "timeout")
 		
